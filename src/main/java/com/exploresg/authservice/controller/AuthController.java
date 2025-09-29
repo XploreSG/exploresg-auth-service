375e6048056573cf23464867aab883bca76ed7ed
sree <sreeraj_ec@hotmail.com>
2025-09-30 07:24:08 +0800

    EXPLORE-11: Jwt Integration with Spring Security

--- src/main/java/com/exploresg/authservice/controller/AuthController.java
+++ src/main/java/com/exploresg/authservice/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.exploresg.authservice.controller;
 
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class AuthController {
@@ -14,5 +18,15 @@ public class AuthController {
         System.out.println("Received token: " + token);
         // You can return a simple response for now
         return ResponseEntity.ok("Token logged successfully");
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getMe(@AuthenticationPrincipal Jwt jwt) {
+        String email = jwt.getClaim("email");
+        String sub = jwt.getSubject();
+        System.err.println("SSO email: " + email + " google sub: " + sub);
+
+        return ResponseEntity.ok(
+                java.util.Map.of("email", email, "sub", sub, "jwt", jwt.getTokenValue()));
     }
 }
